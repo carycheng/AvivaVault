2809ff0e01110b32519b67e6fc5214f4b11f5ea6
Chad Burnette <chadburnette@me.com>
2015-05-06 11:05:45 -0400

    building up examples

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 get "/" do
   #this is an example of using memcached; set to expire in 60 seconds
   @content = settings.cache.fetch('hello-world-message', 60) do
-    "Hello World from Memcachier!"
+    "Hello World from Memcachier"
   end
 
   @files = admin_client.root_folder_items.files
@@ -11,6 +11,7 @@ end
 
 get "/doc/:doc_id" do
   @file = admin_client.file(params['doc_id'])
+  ap @file
   haml :doc
 end
 
