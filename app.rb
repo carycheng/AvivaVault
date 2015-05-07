ffcdb61ae50d3cb1145a66266b916f0e8c7e4260
Chad Burnette <chadburnette@me.com>
2015-05-07 12:53:38 -0400

    switched to JWT

--- app.rb
+++ app.rb
@@ -15,6 +15,9 @@ get "/doc/:doc_id" do
   haml :doc
 end
 
+
+
+#Auth0 actions
 get "/auth0/callback" do
   session[:userinfo] = request.env["omniauth.auth"]
   redirect to('/')
@@ -28,6 +31,16 @@ end
 
 private
 
+running_dir = File.dirname(__FILE__)
+running_dir = Dir.pwd if (running_dir == '.')
+PRIVATE_KEY = OpenSSL::PKey::RSA.new File.read("#{running_dir}/#{ENV['JWT_SECRET_KEY_PATH']}"), ENV['JWT_SECRET_KEY_PASSWORD']
+TOKEN_TTL = 2700 #45 minutes
+
 def admin_client
-  Boxr::Client.new
+  access_token = settings.cache.fetch("box_tokens/enterprise", TOKEN_TTL) do
+    puts "getting new enterprise token"
+    response = Boxr::get_enterprise_token(PRIVATE_KEY)
+    response.access_token
+  end
+  Boxr::Client.new(access_token)
 end
